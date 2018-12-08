685b2f10a99671c53e16508cdc5b7290d9b63bdb
Marcos <marcos@Marcoss-iMac.local>
2018-12-08 19:51:28 +0100

    Created show navigation in Main.storyboard

---
+++ ZooKids/ZooKids/Recuperar_Contrasenia_ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Recuperar_Contrasenia_ViewController.swift
+//  ZooKids
+//
+//  Created by Marcos on 08/12/2018.
+//  Copyright © 2018 ual. All rights reserved.
+//
+
+import UIKit
+
+class Recuperar_Contrasenia_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Volver Atrás", style: .Plain, target: nil, action: nil)
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
