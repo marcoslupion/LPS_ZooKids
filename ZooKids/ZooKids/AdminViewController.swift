ccc150c04e2f2eb99e92359a8047b31b42801798
Alberto Fuentes <alberto@Albertos-Mac.local>
2018-12-17 01:18:10 -0800

    navegacion para cerrar sesion. agregado identificador storyboard main

---
+++ ZooKids/ZooKids/AdminViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AdminViewController.swift
+//  ZooKids
+//
+//  Created by Alberto Fuentes on 17/12/18.
+//  Copyright © 2018 ual. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func irAinicio(sender: UITapGestureRecognizer) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewControllerWithIdentifier("main") as UIViewController
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
